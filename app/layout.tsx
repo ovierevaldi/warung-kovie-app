4f7e8d463b65c1ca521dfed8aca75f6458226a94
Ovie Revaldi <ovierevaldi@gmail.com>
2025-07-08 09:34:01 +0700

    Change Font Family

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Lexend} from "next/font/google";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -7,8 +7,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const lexendFont = Lexend({
   subsets: ["latin"],
 });
 
@@ -25,7 +24,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${lexendFont.className} antialiased`}
       >
         {children}
       </body>
